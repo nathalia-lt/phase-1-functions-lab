298fdceda33b014b8a9e527b07138ef41693bf8c
nathalia <nathaliatroina@gmail.com>
2022-01-16 17:55:29 -0500

    commit tree ed22f2b845a6e8fddd2869e996f9f3e2ffd56a83

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+
+
+function distanceFromHqInBlocks(location) { 
+    const myLocation = 42
+    if(location <= myLocation){
+        return myLocation - location
+    } else{
+        return location -myLocation
+    }
+};
+
+function distanceFromHqInFeet(location){
+   const blockInFeet = 264
+   // we are at 42 and wanna go to 43 and the distance in feet is 264
+   // in this test
+   const blocks = distanceFromHqInBlocks(location)
+   return blocks * blockInFeet
+}
+
+function distanceTravelledInFeet(start, end){
+    const inFeet = 264
+    let distance
+    if(start >= end){
+        distance = start - end 
+    }else{
+        distance = end - start
+    }
+    return distance * inFeet
+}
+
+ function calculatesFarePrice(start, destination){
+      const distance = distanceTravelledInFeet(start, destination)
+      const perFoot = 2
+      if(distance <= 400){
+          return 0
+      }
+ }
